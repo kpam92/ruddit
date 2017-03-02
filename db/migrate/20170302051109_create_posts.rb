85f947f975fbebe58875e4e56f0a9fc279b1465e
kpam92 <kpam1992@gmail.com>
2017-03-01 21:17:08 -0800

    more schema for post

--- db/migrate/20170302051109_create_posts.rb
+++ db/migrate/20170302051109_create_posts.rb
@@ -1,6 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[5.0]
   def change
     create_table :posts do |t|
+      t.string :title, null:false
+      t.text :body
+      t.string :image_url
+      t.integer :author_id, null: false
 
       t.timestamps
     end
