94f9e8dc54ebaa1f8c680150b6b397cd2e0a3be9
kpam92 <kpam1992@gmail.com>
2017-03-02 19:58:27 -0800

    final link_post migration changes

--- db/migrate/20170303035415_create_link_posts.rb
+++ db/migrate/20170303035415_create_link_posts.rb
@@ -3,7 +3,10 @@ class CreateLinkPosts < ActiveRecord::Migration[5.0]
     create_table :link_posts do |t|
       t.string :title, null: false
       t.string :link_url, null: false
+      t.integer :author_id, null: false
+      t.integer :vote, null: false
       t.timestamps
     end
+      add_index :link_posts, :author_id
   end
 end
