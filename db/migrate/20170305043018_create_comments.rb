12dba788cfb78790f44fb865571c5769ebafb809
kpam92 <kpam1992@gmail.com>
2017-03-04 20:38:31 -0800

    indexing author and comment ids

--- db/migrate/20170305043018_create_comments.rb
+++ db/migrate/20170305043018_create_comments.rb
@@ -4,7 +4,10 @@ class CreateComments < ActiveRecord::Migration[5.0]
       t.integer :author_id, null: false
       t.integer :post_id, null: false
       t.integer :comment_id
+      t.text :body, null: false
       t.timestamps
     end
+    add_index :comments, author_id
+    add_index :comments, comment_id
   end
 end
