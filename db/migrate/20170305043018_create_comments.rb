5b65570ad8ee188d23bca2a67966f018a057b8a3
kpam92 <kpam1992@gmail.com>
2017-03-04 20:34:21 -0800

    creating comments table

--- db/migrate/20170305043018_create_comments.rb
+++ db/migrate/20170305043018_create_comments.rb
@@ -1,7 +1,9 @@
 class CreateComments < ActiveRecord::Migration[5.0]
   def change
     create_table :comments do |t|
-
+      t.integer :author_id, null: false
+      t.integer :post_id, null: false
+      t.integer :comment_id
       t.timestamps
     end
   end
