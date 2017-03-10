0c9a7bcc72eaffa7003fd4fd9713aa25202e2076
kpam92 <kpam1992@gmail.com>
2017-03-09 20:51:49 -0800

    user model ;

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   validates :username, :session_token, :password_digest, null: false
   validates :username, :session_token, uniqueness: true
+  validates :password, length: {minimum: 6, allow_nil: true}
+
+  attr_reader :password
+
+  after_initialize: :ensure_session_token
 end
