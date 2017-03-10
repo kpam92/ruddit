a9b07b162da492ddd7340781cc006a6bdb1191dd
kpam92 <kpam1992@gmail.com>
2017-03-09 20:44:27 -0800

    users controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username,:password)
+  end
+
 end
