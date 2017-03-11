1044ae70c7e060700a6dc8b421e26dc3c5543959
kpam92 <kpam1992@gmail.com>
2017-03-10 23:29:21 -0800

    valid password

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,4 +17,8 @@ class User < ApplicationRecord
     @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
+
+  def valid_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
 end
