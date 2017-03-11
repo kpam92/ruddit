1b9a148ab4163c54c20b81ff2335f62c93e9fa05
kpam92 <kpam1992@gmail.com>
2017-03-10 23:31:57 -0800

    ensure session token

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,4 +21,11 @@ class User < ApplicationRecord
   def valid_password?(password)
     BCrypt::Password.new(self.password_digest).is_password?(password)
   end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64(16)
+  end
+  
 end
