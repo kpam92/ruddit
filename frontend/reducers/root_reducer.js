ae2f72bbb4a9826085e147a6f8bbed572a0b3f86
kpam92 <kpam1992@gmail.com>
2017-03-15 20:37:12 -0700

    root reducer changes

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -2,10 +2,10 @@ import {combineReducers} from 'redux';
 
 import SessionReducer from './session_reducer';
 import CommentReducer from './comment_reducer';
-import PhotoReducer from './photo_reducer';
-import AlbumReducer from './album_reducer';
+// import PhotoReducer from './photo_reducer';
+// import AlbumReducer from './album_reducer';
 import UserReducer from './user_reducer';
-import SearchReducer from './search_reducer';
+// import SearchReducer from './search_reducer';
 
 export const RootReducer = combineReducers({
   session: SessionReducer,
