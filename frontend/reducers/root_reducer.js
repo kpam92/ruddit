c3cbc1086dd7aa0da82dcb55511912a51778b88f
kpam92 <kpam1992@gmail.com>
2017-03-15 20:37:39 -0700

    more changes

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,7 +1,7 @@
 import {combineReducers} from 'redux';
 
 import SessionReducer from './session_reducer';
-import CommentReducer from './comment_reducer';
+// import CommentReducer from './comment_reducer';
 // import PhotoReducer from './photo_reducer';
 // import AlbumReducer from './album_reducer';
 import UserReducer from './user_reducer';
