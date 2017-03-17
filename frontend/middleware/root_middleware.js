e183b9541bc065cb55dc991e240109bb18ba3320
kpam92 <kpam1992@gmail.com>
2017-03-16 21:33:33 -0700

    root middleware

---
+++ frontend/middleware/root_middleware.js
@@ -0,0 +1,12 @@
+import {applyMiddleware} from 'redux';
+import SessionMiddleware from './session_middleware';
+
+import UserMiddleware from './user_middleware';
+
+const RootMiddleware = applyMiddleware(
+  SessionMiddleware,
+  UserMiddleware
+
+);
+
+export default RootMiddleware;
