e11c224e142d8a1f569dafc31b93836bce0235c9
kpam92 <kpam1992@gmail.com>
2017-03-16 21:32:06 -0700

    store

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,3 +1,10 @@
 import {createStore} from 'redux';
 import RootReducer from '../reducers/root_reducer';
 import RootMiddleware from '../middleware/root_middleware';
+
+export const configureStore = preloadedState => (
+  createStore(
+    RootReducer,
+  
+  )
+);
