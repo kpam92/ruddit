dfa69afdc1164a6e4ca98f20bb35f7988c091d5a
kpam92 <kpam1992@gmail.com>
2017-03-18 23:16:22 -0700

    reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -4,7 +4,6 @@ import SessionReducer from './session_reducer';
 // import CommentReducer from './comment_reducer';
 // import PhotoReducer from './photo_reducer';
 import UserReducer from './user_reducer';
-// import SearchReducer from './search_reducer';
 
 export const RootReducer = combineReducers({
   session: SessionReducer,
