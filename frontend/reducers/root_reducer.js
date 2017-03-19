286d5ba8b9b8f7560c2c94adbfb05cb6bd93dcd5
kpam92 <kpam1992@gmail.com>
2017-03-18 23:15:48 -0700

    reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -3,7 +3,6 @@ import {combineReducers} from 'redux';
 import SessionReducer from './session_reducer';
 // import CommentReducer from './comment_reducer';
 // import PhotoReducer from './photo_reducer';
-// import AlbumReducer from './album_reducer';
 import UserReducer from './user_reducer';
 // import SearchReducer from './search_reducer';
 
