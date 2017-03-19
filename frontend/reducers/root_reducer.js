81e83ffdb0dc0d61b829387ee22363d4e1304f7b
kpam92 <kpam1992@gmail.com>
2017-03-18 23:16:59 -0700

    root reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,8 +1,6 @@
 import {combineReducers} from 'redux';
 
 import SessionReducer from './session_reducer';
-// import CommentReducer from './comment_reducer';
-// import PhotoReducer from './photo_reducer';
 import UserReducer from './user_reducer';
 
 export const RootReducer = combineReducers({
