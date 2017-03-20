176268a68bc60647ae3ff01a6da1d52fdd45c70e
kpam92 <kpam1992@gmail.com>
2017-03-19 22:10:59 -0700

    greeting container

--- frontend/components/greeting/greeting_container.js
+++ frontend/components/greeting/greeting_container.js
@@ -4,8 +4,7 @@ import { goToProfile } from '../../actions/user_actions';
 import Greeting from './greeting';
 
 const mapStateToProps = state => ({
-  currentUser: state.session.currentUser,
-  search_results: state.search_results
+  currentUser: state.session.currentUser
 });
 
 const mapDispatchToProps = dispatch => ({
