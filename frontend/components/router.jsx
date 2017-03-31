770919d25dd3e0b9138c79c9bcc4acff361aef4b
kpam92 <kpam1992@gmail.com>
2017-03-30 23:07:11 -0700

    rout

--- frontend/components/router.jsx
+++ frontend/components/router.jsx
@@ -11,7 +11,7 @@ import ProfileContainer from './profile/profile_container';
 // import AlbumFormContainer from './album/album_form_container';
 import SearchContainer from './search/search_container';
 
-import { fetchAllPhotos } from '../actions/photo_actions';
+// import { fetchAllPhotos } from '../actions/photo_actions';
 import { fetchAllUsers } from '../actions/user_actions';
 import { fetchAllAlbums } from '../actions/album_actions';
 import { fetchAllComments } from '../actions/comment_actions';
